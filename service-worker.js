035667088f273bba78f3856b3117dfaf833d1a05
roberton881226-cpu <roberton881226@gmail.com>
2025-11-19 20:47:43 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,33 @@
 // service-worker.js
-const CACHE_NAME = 'rpc-cache-v2';
+const CACHE_NAME = "rpc-cache-v3";
+
 const ASSETS = [
-  '/RecargasPaCuba/',
-  '/RecargasPaCuba/index.html',
-  '/RecargasPaCuba/icon-192.png',
-  '/RecargasPaCuba/icon-512.png',
-  '/RecargasPaCuba/manifest.json'
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))
+      )
     ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
